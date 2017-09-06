f171cf42fce145c6a970d80b848fa47fad597603
Alex M <Martinaj192@potsdam.edu>
2017-09-06 16:56:15 -0400

    Adding hardcoded enemy to the drawing board

--- MapEditor/Manager/EnemySourceManager.cs
+++ MapEditor/Manager/EnemySourceManager.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MapEditor.Helpers;
+using MapEditor.Objects;
+using MapEditor.Enums;
 
 namespace MapEditor.Manager
 {
@@ -26,15 +28,16 @@ namespace MapEditor.Manager
 
         private int tileSizeX;
         private int tileSizeY;
+        private Dictionary<string, List<EnemyObjectInfo>> objectInformation;
 
         public EnemySourceManager()
         {
-
         }
 
         public void Init()
         {
 
+            objectInformation = new Dictionary<string, List<EnemyObjectInfo>>();
             this.tileSizeY = 32;
             this.tileSizeX = 32;
             this.tileSizePaddingX = 33;
@@ -43,6 +46,8 @@ namespace MapEditor.Manager
             this.viewStartY = 0;
             this.viewSizeX = 6;
             this.viewSizeY = 10;
+            objectInformation.Add("Default", new List<EnemyObjectInfo>());
+            objectInformation["Default"].Add(new EnemyObjectInfo() { Type = EnemyType.Basic, Source = new Rectangle(0, 0, 32, 32), Destination = new Rectangle(0, 0, 32, 32) });
 
             position = new Vector2(15, 400);
             sourceWindow = new Rectangle((int)position.X, (int)position.Y, tileSizePaddingX* viewSizeX, tileSizePaddingY* viewSizeY);
@@ -52,6 +57,7 @@ namespace MapEditor.Manager
         public void Load()
         {
             emptyTexture = MapManager.Instance.CreateColorTexture(255, 255, 255, 255);
+            texture = MapManager.Instance.Content.Load<Texture2D>("EnemySpriteSheet");
         }
 
         public void Update(GameTime _gameTime)
@@ -60,6 +66,12 @@ namespace MapEditor.Manager
         }
         public void Draw(SpriteBatch _spriteBatch)
         {
+            foreach(EnemyObjectInfo enm in objectInformation["Default"])
+            {
+                Rectangle drawPosition = new Rectangle((int)position.X + enm.Source.X, (int)position.Y + enm.Source.Y, enm.Source.Width, enm.Source.Height);
+                _spriteBatch.Draw(texture, drawPosition, enm.Source, Color.White);
+            }
+
             _spriteBatch.Draw(emptyTexture, sourceWindow, Color.AliceBlue * .10f);
             SpriteBatchAssist.DrawBox(_spriteBatch, emptyTexture, sourceWindow, Color.Red, 2);
             
---
+++ MapEditor/Objects/EnemyObjectInfo.cs
@@ -0,0 +1,58 @@
+﻿using MapEditor.Enums;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor.Objects
+{
+    class EnemyObjectInfo
+    {
+        public Rectangle Source
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+            }
+        }
+
+        public Rectangle Destination
+        {
+            get
+            {
+                return dest;
+            }
+            set
+            {
+                dest = value;
+            }
+        }
+
+        public EnemyType Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        private Rectangle source;
+        private Rectangle dest;
+        private EnemyType type;
+
+        public EnemyObjectInfo()
+        {
+
+        }
+    }
+}
